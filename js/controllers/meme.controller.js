894b076a821a59f2412a8b575707cd8a2a41a677
omribarouch <omri.barouch@safebreach.com>
2024-03-16 17:14:30 +0200

    Add render meme functionality

--- js/controllers/meme.controller.js
+++ js/controllers/meme.controller.js
@@ -1,9 +1,9 @@
 'use strict'
 
 const TEXT_FONT = 'arial'
-const TEXT_SIZE = '45px'
-const TEXT_LINE_WIDTH = 2
-const TEXT_COLOR = 'darkred'
+const TEXT_SIZE = '45'
+const TEXT_LINE_WIDTH = '2'
+const TEXT_LINE_COLOR = 'darkred'
 const TEXT_FILL_COLOR = 'orange'
 
 let gElCanvas
@@ -17,32 +17,43 @@ function initCanvas() {
 }
 
 function setTextProps(props = {}) {
-    gCtx.lineWidth = props.lineWidth || TEXT_LINE_WIDTH
-    gCtx.strokeStyle = props.strokeStyle || TEXT_COLOR
-    gCtx.fillStyle = props.fillStyle || TEXT_FILL_COLOR
-
     let fontFace = props.fontFace || TEXT_FONT
     let fontSize = props.fontSize || TEXT_SIZE
-    gCtx.font = `${fontSize} ${fontFace}`
+    gCtx.font = `${fontSize}px ${fontFace}`
+
+    gCtx.lineWidth = props.lineWidth || TEXT_LINE_WIDTH
+    gCtx.strokeStyle = props.strokeStyle || TEXT_LINE_COLOR
+    gCtx.fillStyle = props.fillStyle || TEXT_FILL_COLOR
 
     gCtx.textAlign = 'center'
     gCtx.textBaseline = 'middle'
 }
 
 function renderMeme() {
-    drawImage('./assets/images/memes-square/1.jpg')
-    drawText('Noted...', 100, 100)
-}
+    const meme = getMeme('123')
 
-function drawImage(url) {
     const elImg = new Image()
-    elImg.src = url
+    elImg.src = meme.img.url
     elImg.onload = () => {
         gCtx.drawImage(elImg, 0, 0, gElCanvas.width, gElCanvas.height)
-
-        let textPos = getCanvasPosByPercent(50, 20)
-        drawText('Meme text here...', textPos.x, textPos.y)
+        drawLines(meme.lines)
     }
+}
+
+function drawLines(lines) {
+    lines.forEach(line => {
+        const { fontFace, fontSize, lineWidth, strokeStyle, fillStyle } = line
+        setTextProps({
+            fontFace,
+            fontSize,
+            lineWidth,
+            strokeStyle,
+            fillStyle,
+        })
+
+        let textPos = getCanvasPosByPercent(line.posPercent.x, line.posPercent.y)
+        drawText(line.txt, textPos.x, textPos.y)
+    })
 }
 
 function drawText(text, x, y) {
--- js/services/meme.service.js
+++ js/services/meme.service.js
@@ -1,1 +1,82 @@
 'use strict'
+
+let gImgs = [
+    { id: '1', url: './assets/images/memes-square/1.jpg', keywords: ['funny', 'cat'] },
+    { id: '2', url: './assets/images/memes-square/2.jpg', keywords: ['funny', 'cat'] },
+    { id: '3', url: './assets/images/memes-square/3.jpg', keywords: ['funny', 'cat'] },
+    { id: '4', url: './assets/images/memes-square/4.jpg', keywords: ['funny', 'cat'] },
+    { id: '5', url: './assets/images/memes-square/5.jpg', keywords: ['funny', 'cat'] },
+    { id: '6', url: './assets/images/memes-square/6.jpg', keywords: ['funny', 'cat'] },
+    { id: '7', url: './assets/images/memes-square/7.jpg', keywords: ['funny', 'cat'] },
+    { id: '8', url: './assets/images/memes-square/8.jpg', keywords: ['funny', 'cat'] },
+    { id: '9', url: './assets/images/memes-square/9.jpg', keywords: ['funny', 'cat'] },
+    { id: '10', url: './assets/images/memes-square/10.jpg', keywords: ['funny', 'cat'] },
+    { id: '11', url: './assets/images/memes-square/11.jpg', keywords: ['funny', 'cat'] },
+    { id: '12', url: './assets/images/memes-square/12.jpg', keywords: ['funny', 'cat'] },
+    { id: '13', url: './assets/images/memes-square/13.jpg', keywords: ['funny', 'cat'] },
+    { id: '14', url: './assets/images/memes-square/14.jpg', keywords: ['funny', 'cat'] },
+    { id: '15', url: './assets/images/memes-square/15.jpg', keywords: ['funny', 'cat'] },
+    { id: '16', url: './assets/images/memes-square/16.jpg', keywords: ['funny', 'cat'] },
+    { id: '17', url: './assets/images/memes-square/17.jpg', keywords: ['funny', 'cat'] },
+    { id: '18', url: './assets/images/memes-square/18.jpg', keywords: ['funny', 'cat'] },
+]
+
+let gMemes = [
+    {
+        id: '123',
+        img: getImg('5'),
+        lines: [
+            {
+                txt: 'I sometimes eat Falafel',
+                fontFace: 'arial',
+                fontSize: '30',
+                lineWidth: '1',
+                strokeStyle: 'red',
+                fillStyle: 'yellow',
+                posPercent: {
+                    x: 50,
+                    y: 20
+                }
+            },
+            {
+                txt: 'And I like it',
+                fontFace: 'arial',
+                fontSize: '45',
+                lineWidth: '1',
+                strokeStyle: 'darkblue',
+                fillStyle: 'white',
+                posPercent: {
+                    x: 65,
+                    y: 80
+                }
+            }
+        ]
+    }
+]
+
+let gKeywordSearchCountMap = { 'funny': 12, 'cat': 16, 'baby': 2 }
+
+function getMeme(id) {
+    return gMemes.find(meme => meme.id === id)
+}
+
+function getAllMemes() {
+
+}
+
+function saveMeme() {
+
+}
+
+function deleteMeme(id) {
+
+}
+
+function getImg(id) {
+    return gImgs.find(img => img.id === id)
+}
+
+function _getRandImg() {
+    let randIdx = getRandomInt(0, gImgs.length)
+    return gImgs[randIdx]
+}
