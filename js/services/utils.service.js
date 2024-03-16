b3163ea415e69b023b1be34d79136884738be44b
omribarouch <omri.barouch@safebreach.com>
2024-03-16 15:39:01 +0200

    Add empty meme service

---
+++ js/services/utils.service.js
@@ -0,0 +1,17 @@
+'use strict'
+
+function generateRandId(length) {
+    let result = ''
+    const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+    for (let i = 0; i < length; i++) {
+        const randIdx = getRandomInt(0, chars.length)
+        result += chars[randIdx]
+    }
+    return result
+}
+
+function getRandomInt(min, max) {
+    const minCeiled = Math.ceil(min)
+    const maxFloored = Math.floor(max)
+    return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled)
+}
